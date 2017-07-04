7017695ccdc487868ed6363c63266a6223a9ab2a
Pavlos Stam <pstamat@outlook.com>
2017-07-04 21:19:30 +0300

    BOL Completed, Started DAL

---
+++ NetBeeTicket.DAL/CustomerDb.cs
@@ -0,0 +1,56 @@
+﻿using NetBeeTicket.BOL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBeeTicket.DAL
+{
+    public class EmployeeDb
+    {
+        private NetBeeTicketDBContext db;
+
+        public EmployeeDb()
+        {
+            db = new NetBeeTicketDBContext();
+        }
+        public IEnumerable<Customer> GetALL()
+        {
+            return db.Customers.ToList();
+        }
+        public Customer GetByID(string Id)
+        {
+            return db.Customers.Find(Id);
+        }
+        public void Insert(Customer emp)
+        {
+            db.Customers.Add(emp);
+            Save();
+        }
+        public void Delete(string Id)
+        {
+            Customer emp = db.Customers.Find(Id);
+            db.Customers.Remove(emp);
+            Save();
+        }
+        public void Update(Customer emp)
+        {
+            db.Entry(emp).State = EntityState.Modified;
+            db.Configuration.ValidateOnSaveEnabled = false;
+            Save();
+            db.Configuration.ValidateOnSaveEnabled = true;
+        }
+
+        public Customer GetByEmail(string email)
+        {
+            return db.Customers.Where(x => x.Email == email).FirstOrDefault();
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
---
+++ NetBeeTicket.DAL/NetBeeTicketDBContext.cs
@@ -0,0 +1,20 @@
+﻿using NetBeeTicket.BOL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBeeTicket.DAL
+{
+    public class NetBeeTicketDBContext : DbContext
+    {
+        public NetBeeTicketDBContext():base("NetBeeTicketDB")
+        {
+
+        }
+        public  DbSet<Role> Roles { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
