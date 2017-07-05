c2df972ab5ddc6efe0b1cbd4231d94dd34e20a93
Pavlos Stam <pstamat@outlook.com>
2017-07-05 17:10:05 +0300

    consoleUI added

---
+++ NetBeeTicket.ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using NetBeeTicket.DAL;
+using NetBeeTicket.BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBeeTicket.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RoleDb R = new RoleDb();
+            R.Insert(new Role() { RoleName = "Admin", RoleCode = "A" });
+            R.Save();
+        }
+    }
+}
